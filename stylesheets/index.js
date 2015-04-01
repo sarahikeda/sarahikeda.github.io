a2c6c8e74c8985644b2c84b03f9d847b0f63bfd0
Sarah Ing <sarahikeda@gmail.com>
2015-04-01 11:19:05 -0700

    edit text and nav bar

---
+++ stylesheets/index.js
@@ -0,0 +1,12 @@
+// <script language="javascript" type="text/javascript">
+// <!--
+// function popitup(url) {
+//     newwindow=window.open(url,'name','height=200,width=150');
+//     console.log("hi");
+//     if (window.focus) {newwindow.focus()}
+//       console.log("yoyo");
+//     return false;
+// }
+
+// // -->
+// </script>
