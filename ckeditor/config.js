49df14730c6ce0c174c01817c67470899f07d304
Sarah Ing <sarahikeda@gmail.com>
2018-01-29 12:12:12 -0500

    Attempt to remove all icons

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,20 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-  config.toolbarGroups = [
-    { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-    { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-    { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-    { name: 'forms' },
-    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-    { name: 'links' },
-    { name: 'insert' },
-    { name: 'styles' },
-    { name: 'colors' },
-    { name: 'tools' },
-    { name: 'others' },
-  ];
+
 
   config.floatingtools = 'Basic';
   config.floatingtools_Basic =
