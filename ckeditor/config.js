a3bc05de8416615c4a84e09a173c7e6ac4987900
Sarah Ing <sarahikeda@gmail.com>
2018-01-29 12:25:01 -0500

    experimenting with removing plugins

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,7 +7,20 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
+  config.toolbarGroups = [
+    { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+    { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+    { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+    { name: 'forms' },
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+    { name: 'links' },
+    { name: 'insert' },
+    { name: 'styles' },
+    { name: 'colors' },
+    { name: 'tools' },
+    { name: 'others' },
+  ];
 
   config.floatingtools = 'Basic';
   config.floatingtools_Basic =
@@ -15,6 +28,7 @@ CKEDITOR.editorConfig = function( config ) {
   	['Bold', 'Italic', 'NumberedList', 'BulletedList', 'Link', 'Blockquote', 'Anchor']
   ];
 
+  config.removePlugins = 'document, clipboard, editing, toolbar'
   // The default plugins included in the basic setup define some buttons that
   // are not needed in a basic editor. They are removed here.
   config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript';
