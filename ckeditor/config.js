965595aef8e7bce5d092b8ac8aaee4935e85cc33
Sarah Ing <sarahikeda@gmail.com>
2018-01-29 11:57:02 -0500

    Update config

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,4 +7,25 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+  config.toolbarGroups = [
+    { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+    { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+    { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+    { name: 'forms' },
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+    { name: 'links' },
+    { name: 'insert' },
+    { name: 'styles' },
+    { name: 'colors' },
+    { name: 'tools' },
+    { name: 'others' },
+  ];
+
+  // The default plugins included in the basic setup define some buttons that
+  // are not needed in a basic editor. They are removed here.
+  config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript';
+
+  // Dialog windows are also simplified.
+  config.removeDialogTabs = 'link:advanced';
 };
