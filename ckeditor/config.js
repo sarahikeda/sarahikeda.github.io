02c9d4a4235cfce65d9819235e8c05dba01b6eb7
Sarah Ing <sarahikeda@gmail.com>
2018-02-02 12:46:43 -0500

    Attempt to restore config to default

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,35 +7,4 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-  config.toolbarGroups = [
-    { name: 'document' },
-    { name: 'clipboard',   groups: [ 'clipboard' ] },
-    { name: 'editing' },
-    { name: 'forms' },
-    { name: 'basicstyles'},
-    { name: 'paragraph'},
-    { name: 'links' },
-    { name: 'insert' },
-    { name: 'tools' },
-    { name: 'others' },
-  ];
-
-  config.extraPlugins = 'pastefromexcel';
-
-  config.removePlugins = 'paragraph, forms, editing'
-  // The default plugins included in the basic setup define some buttons that
-  // are not needed in a basic editor. They are removed here.
-  config.removeButtons = 'Unlink,NewPage,Preview,HorizontalRule,SpecialChar,TextColor,BGColor,PageBreak,Maximize, Undo,Cut,Copy,Undo,Redo,Underline,Strike,Subscript,Superscript,Source,Language,Print,Smiley,Flash,Table,Iframe,Preview,Print';
-
-  config.autoGrow_minHeight = 100;
-  config.autoGrow_maxHeight = 600;
-  config.autoGrow_bottomSpace = 50;
-
-  config.floatingtools = 'Basic';
-  config.floatingtools_Basic =
-  [
-    ['Bold', 'Italic', 'NumberedList', 'BulletedList', 'Link', 'Blockquote', 'Anchor']
-  ];
-  // Dialog windows are also simplified.
-  config.removeDialogTabs = 'link:advanced';
 };
