9349acfbdf86c5a748553459dfd5188af23cc514
Sarah Ing <sarahikeda@gmail.com>
2018-02-02 13:37:52 -0500

    Test half of buttons

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,5 +7,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-  config.removeButtons = 'Unlink,NewPage,Preview,PasteText,PasteFromWord,HorizontalRule,SpecialChar,TextColor,BGColor,PageBreak,Maximize,Clipboard, Undo,Cut,Copy,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript,Source,Language,Print,Smiley,Flash,Table,Iframe,Preview,Print';
+  config.removeButtons = 'Maximize,Clipboard, Undo,Cut,Copy,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript,Source,Language,Print,Smiley,Flash,Table,Iframe,Preview,Print';
 };
